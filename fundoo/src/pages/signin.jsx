c0565058e61c3850e5cc93ff5844f485eae19dbb
puja <pujarana1796@gmail.com>
2022-10-25 22:22:41 +0530

    [puja added]-signin page

---
+++ fundoo/src/pages/signin.jsx
@@ -0,0 +1,112 @@
+import React from 'react'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import './signin.css'
+import { useState } from 'react';
+import { login } from '../services/userService';
+
+const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
+const passwordRegex = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&-+=()])([a-zA-Z0-9]*).{8,}$/;
+
+
+function Signin() {
+    const [signinObj, setSigninObj] = useState ({email:"",password:""}) 
+    const [regexObj,setRegexObj] = useState ({emailBorder:false,emailHelper:"",passwordBorder:false,passwordHelper:""})
+    const takeUserName=(event) => {
+        setSigninObj(prevState => ({
+            ...prevState,
+            email: event.target.value
+        })) 
+        console.log(event.target.value)
+    }
+    const takePassword=(event) => {
+        setSigninObj(prevState => ({
+            ...prevState,
+            password: event.target.value
+        })) 
+        console.log(event.target.value)
+
+    }
+    const submit = () => {
+        let emailTest=emailRegex.test(signinObj.email)
+        let passwordTest=passwordRegex.test(signinObj.password)
+        if (emailTest===false) {
+            setRegexObj(prevState => ({
+                ...prevState,
+                emailBorder:true,
+                emailHelper:"enter valid emails"
+            }))
+        }
+        else if (emailTest===true){
+            setRegexObj(prevState => ({
+                ...prevState,
+                emailBorder:false,
+                emailHelper:""
+            }))
+        
+        }
+        if (passwordTest===false) {
+            setRegexObj(prevState => ({
+                ...prevState,
+                passwordBorder:true,
+                passwordHelper:"incorrect password"
+            }))
+        }
+        else if (passwordTest===true){
+            setRegexObj(prevState => ({
+                ...prevState,
+                passwordBorder:false,
+                passwordHelper:""
+            }))
+        
+        }
+        console.log(signinObj)
+        if(emailTest===true && passwordTest===true){
+            login(signinObj).then((response) => {
+                console.log(response);
+                localStorage.setItem("token",response.data.id)
+
+
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+            console.log("login success")
+
+        }
+    }
+    
+    return (
+    
+            <div class="signinContainer">
+                <div class="container1">
+                <div class="child1"> <img src='images/logo.png' style={{ width: "100px", height: "50px", flexDirection: "column" }} /> </div>
+                    <div class="child2"><h2> Sign in </h2></div>
+                    <div class="child3"> <h3>Use your Google Account</h3> </div>
+                </div>
+                <div class="container2">
+                
+                <TextField fullWidth label="Email or phone" id="Email" autoComplete='off'
+                onChange={takeUserName} error={regexObj.emailBorder} helperText={regexObj.emailHelper}/>
+                        <a>Forgot email?</a>
+                       
+                <TextField fullWidth label="Password" id="Password" 
+                onChange={takePassword} error={regexObj.passwordBorder} helperText={regexObj.passwordHelper}/>
+                    <div class="child4">
+                        Not your computer? Use Guest mode to sign in privately <br />
+                        <a href=" " >Learn More</a>
+                    </div>
+                    <div class="child5">
+                        <Button variant="text">Create account</Button>
+
+                        <Button variant="contained"
+                         onClick={submit}
+                         >Next</Button>
+                    </div>
+                </div>
+            </div>
+        
+    )
+}
+
+export default Signin
---
+++ fundoo/src/services/userService.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+export const login=(loginObj) => {
+    let response = axios.post("http://fundoonotes.incubation.bridgelabz.com/api/user/login",loginObj)
+    console.log("login in process")
+    return response
+}
+export const signUp = async (signupobj) => {
+    let response =  axios.post("http://fundoonotes.incubation.bridgelabz.com/api/user/userSignUp", signupobj)
+    console.log(" processing")
+    return response;
+}
