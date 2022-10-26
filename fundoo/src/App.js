59b94f62dbb28460ef6c00b49532a6466a60f90e
puja <pujarana1796@gmail.com>
2022-10-26 19:48:28 +0530

    [puja added]-creating header and takenote1 and takenote2

--- fundoo/src/App.js
+++ fundoo/src/App.js
@@ -2,12 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Signin from './pages/signin1/signin';
 import Signup from './pages/signup/Signup';
+import Header from './component/Header/header';
+import TakeNote1 from './component/Takenote1/takenote1';
+import TakeNote2 from './component/Takenote2/takenote2';
 
 function App() {
   return (
     <div className="App">
-     <Signin />
-     <Signup />
+      <Header />
+      <TakeNote1 />
+      <TakeNote2 />
+     {/* <Signin />
+     <Signup /> */}
  
     </div>
   );
